525a36c6f44f8f99a472cb5e427e2f712830a14f
LiFeng981 <55880972+LiFeng981@users.noreply.github.com>
2019-11-20 18:04:41 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vueRouter from 'vue-router'
-import router from './router'
-Vue.use(vueRouter);
+import VueRouter from 'vue-router'
+import router from '../router'
+
+Vue.use(VueRouter)
+
+Vue.prototype.$eventBus = new Vue()
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
-  router
 }).$mount('#app')
